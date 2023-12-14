31e887f940707c8a8f5d4300dccc761c498fbbba
Samuel Livio <livio5261@gmail.com>
2023-12-14 09:45:49 +0300

    done

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine,Column, Integer,String
+from sqlalchemy import create_engine,Column, Integer,String,ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
 Base = declarative_base()
 # engine = create_engine('mysql+mysqlconnector://root:Freemo@254#>@localhost:3306/jaribu1')
 engine = create_engine('sqlite:///Restaurants.db')
@@ -16,14 +18,11 @@ class Customers(Base):
         self.id = customer_id
         self.first_name = first_name
         self.last_name = last_name
-        
+    reviews =  relationship('Review',back_populates= 'customer')  
+    restaurants = relationship('Review', back_populates ='customers')  
     def __repr__(self):
         pass
     
-       
-
-
-
 class Restaurants(Base):
     __tablename__ = 'Restaurants' 
     Restaurants_id = Column (Integer(),primary_key=True)  
@@ -34,15 +33,32 @@ class Restaurants(Base):
         self.Restaurants_id = Restaurants_id
         self.name = name
         self.price = price
-        
+    reviews =  relationship('Review',back_populates= 'restaurant') 
+    customers = relationship('Review', back_populates ='restaurants') 
     def __repr__(self):
         pass
-        
+
+class Review(Base):
+    __tablename__ ='Reviews'
+    Review_id =Column (Integer(), primary_key=True)
+    customer_id = Column (Integer(),ForeignKey(Customers.customer_id))
+    Restaurants_id = Column (Integer(),ForeignKey(Restaurants.Restaurants_id))
     
+    customer = relationship ('Customers', back_populates= 'reviews')
+    restaurant = relationship ('Restaurants', back_populates= 'reviews')
+    
+    customers = relationship('Customers' ,back_populates='restaurants')
+    restaurant = relationship ('Restaurants', back_populates= 'customers')
+    
+    
+
+
     
     
     
 Base.metadata.create_all(engine) 
 Session = sessionmaker(bind=engine)
 session = Session()
+
+
 
